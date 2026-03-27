0915808d80158e985e5626479f54b6f93ce1d9fd
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-03-27 10:31:51 -0600

    primer main

---
+++ main.py
@@ -0,0 +1,62 @@
+import sys
+
+
+def main():
+    productos = {}
+
+    primera_linea = True
+    for linea in sys.stdin:
+    
+        linea = linea.strip()
+
+        if primera_linea:
+            primera_linea = False
+            continue
+        if not linea:
+            continue
+    
+        partes = linea.split(',')
+    
+        if partes!=4:
+            continue
+
+        fecha = partes[0]
+        producto = partes[1]
+
+        try:
+            cantidad = int(partes[2])
+            precio = float(partes[3])
+        except ValueError:
+            continue
+
+        if producto not in productos:
+            productos[producto] = {
+                "unidades" = 0,
+                "ingreso" = 0.0
+            }
+    
+        productos[producto]["unidades"] += cantidad
+        productos[producto]["ingreso"] += precio * cantidad
+
+    for producto in productos:
+        unidades = productos[producto]["unidades"]
+        ingreso = productos[producto]["ingreso"]
+        promedio = productos[producto]["promedio"] = ingreso/unidades if unidades >0 else 0
+    
+
+    
+    productos_ordenados = sorted(
+        productos.items(),
+        key=lambda x: x[1]["ingreso"],
+        reverse=True
+    )
+
+    print("producto,unidades_vendidas,ingreso_total,precio_promedio")
+    for nombre, datos in productos_ordenados:
+        print(f"{nombre},{datos[\'unidades\']},{datos[\'ingreso\']:.2f},{datos[\'promedio\']:.2f}")
+
+
+
+
+if __name__ == "__main__":
+    main()
