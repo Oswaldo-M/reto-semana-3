83cc219e0a9dcc152188b0772ad46256e84a5ba5
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-03-27 11:03:07 -0600

    Prueba de acumular cantidades e ingreso por producto, ignorando lineas invalidas

--- main.py
+++ main.py
@@ -15,14 +15,26 @@ for linea in lineas[1:]:  # Desde la segunda linea
     partes = linea.split(',')
     
     #comprueba que haya cuatro columnas
-    if len(partes) == 4:
-        fecha = partes[0]
-        producto = partes[1]
-        #ignorando cantidades y precios invalidos
-        try:
-            cantidad = int(partes[2])
-            precio = float(partes[3])
-        except ValueError:
-            continue
-        
-    print(f"Partes: {partes}")
+    if len(partes) != 4:
+        continue
+    fecha = partes[0]
+    producto = partes[1]
+    #ignorando cantidades y precios invalidos
+    try:
+        cantidad = int(partes[2])
+        precio = float(partes[3])
+    except ValueError:
+        continue
+    if producto not in productos:
+        productos[producto] = {
+            "unidades": 0,
+            "ingreso": 0.0
+        }
+    "Acumulando"
+    productos[producto]["unidades"] += cantidad
+    productos[producto]["ingreso"] += cantidad * precio
+
+print(productos)
+
+
+    
